451457830cdd653a6c86e94da26329d8281582b0
vornsaran1111 <saran529@gmail.com>
2025-02-12 09:10:08 +0700

    init project

---
+++ src/test/java/com/domnerka/DomnerkaBpmnServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domnerka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomnerkaBpmnServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
